52a30b2fb14b3021fd1d99d462ecd6f191864645
Jackson Petty <jacksonp@jackson-petty-macbook-pro.tail8a69.ts.net>
2026-05-13 09:50:33 -0700

    base dir again

--- test_interactive.sh
+++ test_interactive.sh
@@ -4,4 +4,5 @@ beaker session create \
     --cluster ai2/saturn \
     --gpus 1 \
     --min-runtime 2h \
-    --mount src=weka,ref=reviz-default,dst=/weka
+    # --mount src=weka,ref=reviz-default,dst=/weka \
+    --mount src=weka,ref=oe-training-default,dst=/weka/oe-training-default
